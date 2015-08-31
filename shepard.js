b23a5d413991137912deada509f484c55962ccbf
David Nääs <david_naas@hotmail.com>
2015-08-31 23:16:25 +0200

    Experimentally lower frequency range

--- shepard.js
+++ shepard.js
@@ -5,29 +5,32 @@
   }catch(e){
     alert('Web audio does not work with your browser');
   }
-  const STARTFREQ = 5000;
+  // TODO understand why these arbitrary values work so well
+  const STARTFREQ = 800;
   var mean = STARTFREQ/2;
-  // All freuencies equally likely
   var sigma = mean / 10;
+
+  // Patching of web audio nodes
   var mainGain = context.createGain();
-  mainGain.gain.value = 0.1;
+  mainGain.gain.value = 0.25;
   mainGain.connect(context.destination);
   var oscillators = populateOscillators();
-  window.setInterval(playShepard, 1);
+  window.setInterval(playShepard, 2);
   
 
   function populateOscillators() {
-    var arr = new Array(STARTFREQ/1000);
+    var arr = new Array(STARTFREQ/100);
    
     for(var i = 0; i < arr.length; i++) {
-       var tmpOsc = context.createOscillator();
-       var tmpGain = context.createGain();
-       tmpOsc.frequency.value = (i + 1) * 1000;
-       tmpOsc.start(1);
-       tmpOsc.connect(tmpGain);
-       tmpGain.connect(mainGain);
-       arr[i] = {"osc": tmpOsc,
-                  "gain": tmpGain};
+      var tmpOsc = context.createOscillator();
+      var tmpGain = context.createGain();
+      tmpOsc.frequency.value = (i + 1) * 100;
+      tmpOsc.type = "sine";
+      tmpOsc.start(1);
+      tmpOsc.connect(tmpGain);
+      tmpGain.connect(mainGain);
+      arr[i] = {"osc": tmpOsc,
+                "gain": tmpGain};
     };
   
     return arr;
@@ -36,15 +39,11 @@
   function playShepard(){
     oscillators.forEach(function(oscObj, index){
       if(oscObj.osc.frequency.value > 40){
-        oscObj.osc.frequency.value -= 0.2;
+        oscObj.osc.frequency.value -= 0.05;
       }else{
         oscObj.osc.frequency.value = STARTFREQ;
       }
-      if(index === 1)
-        console.log(oscObj.osc.frequency.value);
-
-      oscObj.gain.gain.value = freqToVolume(oscObj.osc.frequency.value) * 1000;
-      //console.log(oscObj.gain.gain.value);
+      oscObj.gain.gain.value = freqToVolume(oscObj.osc.frequency.value) * 100;
     });
   }
 
